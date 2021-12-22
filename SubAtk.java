3a10a11a7b0d78bbbcf8e1e1115545975454a6cb
kani-hirotaka19 <kani.hirotaka.19@shizuoka.ac.jp>
2021-12-22 14:05:48 +0900

    SubAtk.javaを実装しました

--- SubAtk.java
+++ SubAtk.java
@@ -2,10 +2,9 @@ public class SubAtk {
 	/// <summary>
 	/// 引数をデクリメントして返すメソッドを作ろう！
 	/// </summary>
-        atk --;
-        return atk;
 
 	public int subatk(int atk) {
-		return 1;
+                atk --;
+		return atk;
 	}
 }
